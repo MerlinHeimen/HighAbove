a598b67f41c9c66eee25495669d0d4ab74e2cc3b
MerlinHeimen <116766525+MerlinHeimen@users.noreply.github.com>
2023-06-25 22:34:37 +0200

    Platforms now only starts moving when the player stands on them

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -6,7 +6,7 @@ public class PlatformMovement : MonoBehaviour
 {
 
     [SerializeField] private WaypointPath _waypointPath;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _speed = 4.0f;
 
     private int _targetWaypointIndex;
 
@@ -15,24 +15,46 @@ public class PlatformMovement : MonoBehaviour
 
     private float _timeToWaypoint;
     private float _elapsedTime;
+    private float _platformRevalations;
+
+    [SerializeField] private bool isParent = false;
 
     // Start is called before the first frame update
     void Start()
     {
         TargetNextWaypoint();
+        _platformRevalations = 2.0f;
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
-        _elapsedTime += Time.deltaTime;
-
+        //Platform functionality
         float elapsedPercentage = _elapsedTime / _timeToWaypoint;
+
+        if (isParent == true)
+        {
+            _elapsedTime += Time.deltaTime;
+        }
+        else
+        {
+            _elapsedTime = 0;
+        }
+
+        //Platform interpolation between waypoints
         transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
 
         if (elapsedPercentage >= 1)
         {
             TargetNextWaypoint();
+            _platformRevalations++;
+        }
+
+        //Checks if platform has moved back to the starting position
+        if (_platformRevalations == 2.0f)
+        {
+            _platformRevalations = 0.0f;
+            isParent = false;
         }
     }
 
@@ -52,6 +74,7 @@ public class PlatformMovement : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         other.transform.SetParent(transform);
+        isParent = true;
     }
 
     private void OnTriggerExit(Collider other)
